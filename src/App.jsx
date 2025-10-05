a358aba4aae7e5bf8506f3251ecd68ca219b6b1b
balintbenczik <balintvegyesen@gmail.com>
2025-10-05 22:29:20 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <div className='container border-2 border-amber-600 py-3 px-3 bg-amber-200'>
+    <h1 className='text-3xl text-blue-500 border-2'>Container</h1>
+  </div>
+  )
+}
+
+export default App
