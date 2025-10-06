10300cf49c8a74c87a154a52bb0106fda7710bb5
balintbenczik <balintvegyesen@gmail.com>
2025-10-06 17:00:25 +0200

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import './App.css'
 
 function App() {
 
-  return (<>
-    <div className='flex gap-5 justify-center'>
-      <div className=' w-1/2 container border-2 border-amber-600 py-3 px-3 bg-amber-200'>
-        <h1 className='text-3xl text-blue-500 border-2'>Container</h1>
-      </div>
-      <div className=' w-1/6 container border-2 border-amber-600 py-3 px-3 bg-amber-200'>
-        <h1 className='text-3xl text-blue-500 border-2'>Container</h1>
-      </div>
-    </div>
-  </>)
+  return (
+    <>
+    
+    </>
+  );
 }
+
+
+
 
 export default App
